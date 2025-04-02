f1fb93768ebee4fa80c50f6d13cf19d149c987e3
antking19 <cuong.truongkien@gmail.com>
2025-04-02 11:50:26 +0700

    luu y ve su dung key voi list

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import "./App.css";
 import TodoItem from "./components/TodoItem";
 
 function App() {
+    const todoList = [
+        { id: 1, name: "Đi học thêm" },
+        { id: 2, name: "Đi học võ" },
+        { id: 3, name: "Đi ngủ" },
+    ];
+
+    const todos = todoList.map((todo) => {
+        return <TodoItem name={todo.name} key={todo.id} />;
+    });
+
     return (
-        <div>
-            <input type="text" name="add-new-task" placeholder="Add new task" />
-            <div>
-                <TodoItem name="Đi học thêm" />
-                <TodoItem name="Đi học võ" />
-                <TodoItem name="Đi ngủ" />
-            </div>
+        <div className="container">
+            <input
+                type="text"
+                name="add-new-task"
+                placeholder="Add new task"
+                className="task-input"
+            />
+            <div>{todos}</div>
         </div>
     );
 }
