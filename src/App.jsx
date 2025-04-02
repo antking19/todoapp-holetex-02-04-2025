edb4f0bea35562b95d03e3be8b60a63d03cbbfcd
antking19 <cuong.truongkien@gmail.com>
2025-04-02 11:30:45 +0700

    truyen du lieu vao component bang props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-    return <p>Todo App</p>;
+    return (
+        <div>
+            <input type="text" name="add-new-task" placeholder="Add new task" />
+            <div>
+                <TodoItem name="Đi học thêm" />
+                <TodoItem name="Đi học võ" />
+                <TodoItem name="Đi ngủ" />
+            </div>
+        </div>
+    );
 }
 
 export default App;
